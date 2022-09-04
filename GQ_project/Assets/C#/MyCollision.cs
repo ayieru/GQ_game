c237593b759ed78014e38c01aabee2e445ad4030
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2022-09-05 06:59:48 +0900

    update

--- GQ_project/Assets/C#/MyCollision.cs
+++ GQ_project/Assets/C#/MyCollision.cs
@@ -8,12 +8,14 @@ public class MyCollision : MonoBehaviour
     private GameObject obj;
 
     private Vector3 pos;
+    private int vc;
 
     private void Start()
     {
         obj = GameObject.Find("PlayerCapsule").transform.GetChild(3).gameObject;
 
         sk = obj.GetComponent<SibaKari>();
+        vc = sk.VerticsCount;
 
         pos = transform.position;
     }
@@ -32,31 +34,17 @@ public class MyCollision : MonoBehaviour
     {
         Vector2 vec1;
         Vector2 vec2;
-        float[] cross_z = new float[4];
-        
-        for (int i = 0; i < 4; i++)
+        float[] cross_z = new float[vc];
+
+        for (int i = 0; i < vc; i++)
         {
-            if (i == 3)
-            {
-                vec1.x = sk.pos[i].x - pos.x;
-                vec1.y = sk.pos[i].z - pos.z;
+            vec1.x = sk.pos[i].x - pos.x;
+            vec1.y = sk.pos[i].z - pos.z;
 
-                vec2.x = sk.pos[0].x - sk.pos[i].x;
-                vec2.y = sk.pos[0].z - sk.pos[i].z;
+            vec2.x = sk.pos[i + 1].x - sk.pos[i].x;
+            vec2.y = sk.pos[i + 1].z - sk.pos[i].z;
 
-                cross_z[i] = vec2.x * vec1.y - vec2.y * vec1.x;
-                Debug.Log(cross_z[i]);
-            }
-            else
-            {
-                vec1.x = sk.pos[i].x - pos.x;
-                vec1.y = sk.pos[i].z - pos.z;
-
-                vec2.x = sk.pos[i + 1].x - sk.pos[i].x;
-                vec2.y = sk.pos[i + 1].z - sk.pos[i].z;
-
-                cross_z[i] = vec2.x * vec1.y - vec2.y * vec1.x;
-            }
+            cross_z[i] = vec2.x * vec1.y - vec2.y * vec1.x;
         }
 
         if (cross_z[0] < 0 && cross_z[1] < 0 && cross_z[2] < 0 && cross_z[3] < 0) return true;
